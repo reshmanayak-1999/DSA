358f7c9c70a0de1378a2a8cad36e4205f32bdcef
reshmanayak-1999 <reshmanayak1999@gmail.com>
2025-08-27 23:17:22 +0530

    DSA1 problem Count Digit solved

--- src/test/java/CountDigit.java
+++ src/test/java/CountDigit.java
@@ -1,2 +1,11 @@
 public class CountDigit {
+    public static void main(String[] args) {
+        int a =34526;
+        int countDigit=0;
+        while(a>0){
+          a=a/10;
+          countDigit++;
+        }
+        System.out.println(countDigit);
+    }
 }
